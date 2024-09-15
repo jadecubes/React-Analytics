a585076d2166dee65e50b00988acc38bf2dd6499
Debra <debra_juan@moneynet.tw>
2024-09-15 16:59:47 +0800

    feat: add files

--- apps/analyticsApp/vite.config.ts
+++ apps/analyticsApp/vite.config.ts
@@ -1,36 +1,52 @@
-/// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
-import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import svgr from 'vite-plugin-svgr'
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/analyticsApp',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/fortune-panel',
 
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
-  build: {
-    outDir: '../../dist/apps/analyticsApp',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    server: {
+      port: 3000,
+      host: 'localhost',
+      fs: {
+        cachedChecks: false
+      }
     },
-  },
-});
+
+    preview: {
+      port: 4300,
+      host: 'localhost'
+    },
+
+    /**
+     * To fix absolute paths in scss
+     * https://stackoverflow.com/questions/70632978/is-there-a-way-so-i-can-use-absolute-paths-in-scss-using-vite
+     */
+    resolve: {
+      alias: {
+        '~': resolve(__dirname, 'src')
+      }
+    },
+    plugins: [react(), nxViteTsPaths(), svgr()],
+
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+
+    envPrefix: ['REACT_APP_'],
+
+    build: {
+      outDir: '../../dist/apps/fortune-panel',
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true
+      }
+    }
+  }
+})
