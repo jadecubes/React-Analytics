715b88157211b64b5f0ba4f5c9d8a495c3d20b57
Debra <debra_juan@moneynet.tw>
2024-09-15 16:49:36 +0800

    feat: add analyticsApp and configure env

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
-import { getJestProjects } from '@nx/jest';
-
+/* eslint-disable */
 export default {
-  projects: getJestProjects(),
+  displayName: 'analyticsApp',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/analyticsApp',
 };
-
