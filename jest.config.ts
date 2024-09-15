54c16fda3ff7fdc9dcae9d3c8432f763c8a74a85
Debra <debra_juan@moneynet.tw>
2024-09-15 17:44:02 +0800

    fix: errors

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,5 @@
-/* eslint-disable */
+import { getJestProjects } from '@nx/jest';
+
 export default {
-  displayName: 'analyticsApp',
-  preset: '../../jest.preset.js',
-  transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/analyticsApp',
+  projects: getJestProjects(),
 };
